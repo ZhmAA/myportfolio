dddb72166cc0a25d4a30e2ca0d4f9feabcdb4dd3
ZhmAA <azhm@ukr.net>
2016-03-14 21:30:35 +0200

    switch uploading image to file(when production will switch to s3)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',                        
-    aws_access_key_id:     Rails.application.secrets.config_aws_access_key_id,                        
-    aws_secret_access_key: Rails.application.secrets.config_aws_secret_access_key                        
-  }
-  config.fog_directory  = ENV['S3_BUCKET']                         
-end 
+#CarrierWave.configure do |config|
+#  config.fog_credentials = {
+#    provider:              'AWS',                        
+#    aws_access_key_id:     Rails.application.secrets.config_aws_access_key_id,                        
+#    aws_secret_access_key: Rails.application.secrets.config_aws_secret_access_key                        
+#  }
+#  config.fog_directory  = ENV['S3_BUCKET']                         
+#end 
