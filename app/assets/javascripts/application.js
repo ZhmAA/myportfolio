7c5a9195e899b9f378895a776f990cd8b3b221a8
ZhmAA <azhm@ukr.net>
2016-04-25 19:23:53 +0300

    front end fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,20 +17,6 @@
 //= require_tree .
 
 
-//anchor scroll
-$(document).ready(function(){
-      $('#nav').on('click', 'a', function (event) {
-          //отменяем стандартную обработку нажатия по ссылке
-          event.preventDefault();
-          //забираем идентификатор бока с атрибута href
-          var id = $(this).attr('href').replace('/', ''),
-          //узнаем высоту от начала страницы до блока на который ссылается якорь
-              top = $(id).offset().top;
-          //анимируем переход на расстояние - top за 500 мс
-          $('body,html').animate({scrollTop: top}, 500);
-      });
-  });
-
 //
 
 (function($) {
